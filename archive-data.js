a26dfd00da52372199057c8d0c3f87c961c74f6a
Donghyuk Lee <131878690+donghyuklee1@users.noreply.github.com>
2026-06-21 02:09:24 +0900

    Update archive-data.js

--- archive-data.js
+++ archive-data.js
@@ -33,7 +33,7 @@ const ARCHIVE_POSTS_MANIFEST = [
     title: 'Starting the archiving', 
     date: '2026-06-21',
     categories: 'Talk, Daily',
-    excerpt: 'Started archiving my projects and research here.',
+    excerpt: '👾',
     author: 'Donghyuk Lee'
   }
 ];
